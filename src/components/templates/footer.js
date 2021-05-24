afef947d8ebaccaadfd875d2d1a094b8aa9f2647
santosaputro <psysaputro@gmail.com>
2021-05-24 13:26:55 +0700

    change footer content

--- src/components/templates/footer.js
+++ src/components/templates/footer.js
@@ -10,7 +10,7 @@ $("#app-footer").html(`
     <span class="flex-grow-1"></span>
     <div class="d-flex align-items-center">
       <div class="text-right">
-        <p class="m-0">© SIMRS v1.0.0</p>
+        <p class="m-0">© SIMRS v1.2.0</p>
         <p class="m-0">
           Made With <b style="color: brown">&#10084;</b> by X-Solusi
         </p>
